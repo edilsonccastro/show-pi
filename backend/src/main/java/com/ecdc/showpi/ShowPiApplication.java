24ff6c433beeb51d0f5ddf0859db8c4a2cc0d67c
Edilson Castro <edilsoncastro.1980@yahoo.com>
2024-01-26 17:31:26 -0300

    Spring Initializr generated initial code

---
+++ backend/src/main/java/com/ecdc/showpi/ShowPiApplication.java
@@ -0,0 +1,13 @@
+package com.ecdc.showpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowPiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowPiApplication.class, args);
+	}
+
+}
